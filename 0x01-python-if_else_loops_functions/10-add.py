ed681731691b31f552b95df7ffeb01e1e14a9eef
MagadiM <milliemagadi4@gmail.com>
2022-10-25 00:46:59 -0700

    These are loops and functions commits

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    """The function should return the addition of a and b"""
+    return(a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """The function should return a raised to the power of b"""
+    return (a **b)
