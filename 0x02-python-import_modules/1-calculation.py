e0718deaa8b6191981790e43776eb530dd2340b3
MagadiM <milliemagadi4@gmail.com>
2022-12-26 09:09:34 -0800

    second task ^_^

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    a = 10
-    b = 5
+    from calculator_1 import add, sub, mul, div
+    a, b = 10, 5
     print("{} + {} = {}".format(a, b, (a + b)))
     print("{} - {} = {}".format(a, b, (a - b)))
     print("{} * {} = {}".format(a, b, (a * b)))
