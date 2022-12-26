2b170e35db8133122135e7ae25db0e8bc3e065ae
MagadiM <milliemagadi4@gmail.com>
2022-12-26 13:53:12 -0800

    first data structures commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+        """The function will print an integer per line 
+        with one integer per line
+        """
+        for i in my_list:
+            print("{:d}".format(i))
